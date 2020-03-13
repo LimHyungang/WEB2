cae07d473c6c60cc166857d81a4ef6efb994eae9
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-03-13 17:14:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,39 +2,64 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/?id=WEB">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+  for (var i = 0; i < filelist.length; i++) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+  }
+  list = list + '</ul>';
+
+  return list;
+}
+
+var app = http.createServer(function(request,response){  // localhost를 통해 동작시킬 app
     var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    if(_url == '/'){
-      _url = '/index.html';
+    var queryData = url.parse(_url, true).query;  // queryString 정보
+    var pathname = url.parse(_url, true).pathname;
+
+    if (pathname === '/') {  // pathname이 root일 경우 (pathname : queryString제외한 path만 보여줌, path : queryString까지 모두 포함)
+      if (queryData.id === undefined) {  // 기본 페이지
+        fs.readdir('./data', function(error, filelist) {
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
+
+        });
+      } else {
+        fs.readdir('./data', function(error, filelist) {
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    
-    fs readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${queryData.id}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/?id=WEB">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${queryData.id}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    });
 
 });
 app.listen(3000);
