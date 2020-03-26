5ecc16d4c678ce0eab801db6521733750d1f310a
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-03-26 15:28:05 +0900

    topic, author join

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ var app = http.createServer(function(request,response){  // function : 서버로
         db.query(`select * from topic`, function(error, topics) {
           if (error) throw error;
           // ${queryData.id}보다는 쿼리의 ?에 들어갈 값을 다음 매개변수에 배열 형태로 보내주는게 보안상 더 좋다.
-          db.query(`select * from topic where id=?`, [queryData.id], function(error2, topic) {
+          db.query(`select * from topic left join author on topic.author_id=author.id where topic.id=?`, [queryData.id], function(error2, topic) {
             if (error2) throw error2;
             var title = topic[0].title;  // 객체 형태의 튜플이 배열에 담겨서 들어오기 때문에 [0] 해줘야 함
             var description = topic[0].description;
@@ -68,6 +68,9 @@ var app = http.createServer(function(request,response){  // function : 서버로
             <textarea name="description" placeholder="description"></textarea>
           </p>
           <p>
+              ${template.authorSelect(authors)}
+          </p>
+          <p>
             <input type="submit">
           </p>
         </form>`,
@@ -87,7 +90,7 @@ var app = http.createServer(function(request,response){  // function : 서버로
         var post = qs.parse(body);
         db.query(`insert into topic (title, description, created, author_id) 
                   values (?, ?, now(), ?)`, 
-                  [post.title, post.description, 1], 
+                  [post.title, post.description, post.author],  // post.author : template.authorSelect의 <select name="author">중 선택된 <option>의 value
                   function(error, result) {
                     if (error) throw error;
                     response.writeHead(302, {Location : `/?id=${result.insertId}`});
@@ -97,33 +100,40 @@ var app = http.createServer(function(request,response){  // function : 서버로
       });
     } else if (pathname === '/update') {
         db.query(`select * from topic`, function(error, topics) {
-        if (error) throw error;
-        db.query(`select * from topic where id=?`, [queryData.id], function(error2, topic) {
-          if (error2) throw error2;
-          var list = template.list(topics);
-          var html = template.HTML(topic[0].title, list,
-            // <form>에서 입력받은 데이터들을 post방식으로 넘겨준다.
-            `
-            <form action="/process_update" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-          );
-          // 첫번쨰 <input> : 수정할 대상 파일의 이름을 넘겨주기 위함
-          // 두번쨰 <input> value : 기본값
-          // textarea는 기본값을 value 속성이 아닌 태그 사이에 넣어서 설정
-          response.writeHead(200);
-          response.end(html);
+          if (error) throw error;
+          db.query(`select * from topic where id=?`, [queryData.id], function(error2, topic) {
+            if (error2) throw error2;
+            db.query(`select * from author`, function(error3, authors) {
+             if (error3) throw error3;
+              
+             var list = template.list(topics);
+             var html = template.HTML(topic[0].title, list,
+               // <form>에서 입력받은 데이터들을 post방식으로 넘겨준다.
+               `
+                <form action="/process_update" method="post">
+                  <input type="hidden" name="id" value="${topic[0].id}">
+                  <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                 <p>
+                   <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                 </p>
+                 <p>
+                   ${template.authorSelect(authors, topic[0].author_id)}
+                 </p>
+                 <p>
+                   <input type="submit">
+                 </p>
+               </form>
+               `,
+               `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+             );
+              // 첫번쨰 <input> : 수정할 대상 파일의 이름을 넘겨주기 위함
+              // 두번쨰 <input> value : 기본값
+              // textarea는 기본값을 value 속성이 아닌 태그 사이에 넣어서 설정
+              response.writeHead(200);
+              response.end(html);
+            });
+          });
         });
-      });
     } else if (pathname === '/process_update') {  // /update에서 데어터를 넘겨주면서 바로 /process_update로 이동하게 된다.
       // body 정의 ~ request.on() 선언 : post방식으로 넘어온 데이터를 받는 로직
       var body = '';
@@ -132,7 +142,7 @@ var app = http.createServer(function(request,response){  // function : 서버로
       });
       request.on('end', function() {  // 더이상 들어올 데이터가 없을 때 실행되는 콜백 메서드.
         var post = qs.parse(body);
-        db.query(`update t  opic set title=?, description=?, author_id=1 where id=?`, [post.title, post.description, post.id], function(error, result) {
+        db.query(`update t  opic set title=?, description=?, author_id=? where id=?`, [post.title, post.description, post.author, post.id], function(error, result) {
           response.writeHead(302, {Location : `/?id=${post.id}`}); 
           response.end(); 
         });
